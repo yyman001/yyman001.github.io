0d28cdf453643a3f0a993a6bb157bc044e6a2a90
wangdongman <wangdongman@xingfeiinc.com>
2019-03-27 18:00:05 +0800

    ts编译demo

---
+++ demo/ts_demo/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ *
+ *
+ * @param {String} text
+ */
+function test(text) {
+    console.log("ts-demo:" + text);
+}
+test('编译ts测试!不乱码了吧');
---
+++ demo/ts_demo/main.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ *
+ * @param {String} text
+ */
+function test (text: String):void {
+  console.log(`ts-demo:${text}`)
+}
+
+
+
+test('编译ts测试!不乱码了吧')
