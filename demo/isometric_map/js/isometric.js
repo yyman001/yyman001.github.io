6d07e3e543cea2e69295bdb1a03ffaa6ce3b2533
yyman001 <yyman001@qq.com>
2019-07-04 23:15:54 +0800

    update: 更新网格绘制

--- demo/isometric_map/js/isometric.js
+++ demo/isometric_map/js/isometric.js
@@ -25,29 +25,37 @@ function drawRhombus(Graphics, TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH_HALF, TILE_HE
 function initTileMap(container, { TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH_HALF, TILE_HEIGHT_HALF, ROW, COL }) {
     console.table({ TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH_HALF, TILE_HEIGHT_HALF, ROW, COL })
  
+    // 计算网格4个点坐标
+    // 算出 (0,0) 坐标偏移量,移动这个值 - 半个tile 宽度(绘制不是左上角,而是tile中心坐标)
+    let top = {x: ROW * TILE_WIDTH_HALF, y: 0}
+    let right = {x: (ROW + COL) * TILE_WIDTH_HALF, y: COL * TILE_HEIGHT_HALF}
+    let bottom = {x: COL * TILE_WIDTH_HALF, y: ROW * TILE_HEIGHT_HALF + COL * TILE_HEIGHT_HALF}
+    let left = {x: 0, y: ROW * TILE_HEIGHT_HALF}
+
+    console.log('top:', top)
+    console.log('right:', right)
+    console.log('bottom:', bottom)
+    console.log('left:', left)
+
     let isometricContainer = new PIXI.Container()
     let isometricTileGraphics = new PIXI.Graphics()
     let isometricTileGraphicsBorder = new PIXI.Graphics()
 
-    isometricTileGraphics.lineStyle(1, 0xff0000)
+    isometricTileGraphics.lineStyle(1, 0xcccccc)
     isometricTileGraphicsBorder.lineStyle(1, 0xff0000)
 
-    let offsetX = []
-    for (let u = 0; u < COL; u++) {
+    for (let u = 0; u < ROW; u++) {
         let screenPos = mapCoordToScreen({ x: 0, y: u }, TILE_WIDTH_HALF, TILE_HEIGHT_HALF)
         // TODO 修正绘制起点坐标,当前只适合 等比网格垂直居中
-        screenPos.x = screenPos.x + container._width / 2 - TILE_WIDTH_HALF
-        offsetX.push(screenPos.x)
+        screenPos.x = screenPos.x + top.x - TILE_WIDTH_HALF
         console.log('u:', screenPos)
-        for (let v = 0; v < ROW; v++) {
+        for (let v = 0; v < COL; v++) {
             isometricTileGraphics.drawRect(screenPos.x, screenPos.y, TILE_WIDTH, TILE_HEIGHT)
             drawRhombus(isometricTileGraphicsBorder, TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH_HALF, TILE_HEIGHT_HALF, screenPos.x, screenPos.y)
             screenPos.x += TILE_WIDTH_HALF
             screenPos.y += TILE_HEIGHT_HALF
         }
     }
-
-    console.log('offsetX:',offsetX)
 
     isometricTileGraphics.endFill()
     isometricTileGraphicsBorder.endFill()
@@ -66,6 +74,6 @@ function initTileMap(container, { TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH_HALF, TILE
     window.container = container
     window.isometricContainer = isometricContainer
     // !! 修正了非等比网格居中对其
-    isometricContainer.x = 0 - offsetX.pop()
+    // isometricContainer.x = 0 - offsetX.pop()
     
 }
